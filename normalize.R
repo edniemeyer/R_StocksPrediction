8813a5b244bd2edc1334c7e20faf32ff5b263c86
edniemeyer <edniemeyer@gmail.com>
2017-02-14 12:14:46 -0200

    added denormalization

--- normalize.R
+++ normalize.R
@@ -3,6 +3,7 @@ csv_dolar_puro<-read.csv('dados-Dolar.csv')
 csv_ibov_puro<-read.csv('dados-Ibov.csv')
 
 #min max normalization
+
 minMaxNormalize <- function(x) {
   return ((x - min(x)) / (max(x) - min(x)))
 }
@@ -12,24 +13,42 @@ ibov_minMax <- as.data.frame(lapply(csv_ibov_puro,minMaxNormalize))
 
 
 #min max denormaliztion
-minvec <- sapply(csv_dolar_puro,min)
-maxvec <- sapply(csv_dolar_puro,max)
+
+dolarminvec <- sapply(csv_dolar_puro,min)
+dolarmaxvec <- sapply(csv_dolar_puro,max)
 minMaxDenormalize <- function(x,minval,maxval) {
   x*(maxval-minval) + minval
 }
 
-den_dolar_minMax <- as.data.frame(Map(minMaxDenormalize,dolar_minMax,minvec,maxvec))
+den_dolar_minMax <- as.data.frame(Map(minMaxDenormalize,dolar_minMax,dolarminvec,dolarmaxvec))
 
 
 
 #Z score normalization
 
-dolar_Z <- as.data.frame( scale(csv_dolar_puro ))
-ibov_Z <- as.data.frame( scale(csv_ibov_puro ))
+dolarmeanvec <- sapply(csv_dolar_puro,mean)
+dolarsdvec <- sapply(csv_dolar_puro,sd)
+
+zScoreNormalize <- function(x){
+  return((x-mean(x))/sd(x))
+}
+
+dolar_Z <- as.data.frame(lapply(csv_dolar_puro,zScoreNormalize))
+ibov_Z <- as.data.frame(lapply(csv_ibov_puro,zScoreNormalize))
+
+
+#Z score denormalization
+
+zScoreDenormalize <- function(x, meanvec, sdvec){
+  (x*sdvec) + meanvec
+}
+
+
+den_dolar_Z <- as.data.frame(Map(zScoreDenormalize,dolar_Z,dolarmeanvec,dolarsdvec))
+
 
 
 #decimal normalization
-
 
 decimalNormalize <- function(x) {
   return (x / 10^log10(max(x)))
@@ -37,4 +56,14 @@ decimalNormalize <- function(x) {
 
 dolar_decimal <- as.data.frame(lapply(csv_dolar_puro,decimalNormalize))
 ibov_decimal <- as.data.frame(lapply(csv_ibov_puro,decimalNormalize))
+
+
+#decimal denormalize
+
+decimalDenormalize <- function(x,maxvec) {
+  return (x*(10^log10(maxvec)))
+}
+
+den_dolar_decimal <- as.data.frame(Map(decimalDenormalize,dolar_decimal,dolarmaxvec))
+
 
